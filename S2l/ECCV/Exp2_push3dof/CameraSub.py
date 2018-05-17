3ef4ff3f59368affdf8641cf3156c27ae9ff3f6d
ROSKinetic <meetleopauly@yahoo.com>
2018-05-17 21:04:58 +0100

    real exp complete pipeline 1

--- S2l/ECCV/Exp2_push3dof/CameraSub.py
+++ S2l/ECCV/Exp2_push3dof/CameraSub.py
@@ -13,7 +13,8 @@ class CameraSub:
         rospy.init_node('camera_sub_node', anonymous=True)
         self.bridge = CvBridge()
         self.img_msg=Image()
-    
+        self.counter=1       
+ 
     def check(self,data):
         print('callback')
 
@@ -27,7 +28,10 @@ class CameraSub:
             print('error',e)
 
 
-        
+        print(self.counter)
+        self.counter=self.counter+1
+	    
+
         cv2.imshow('recieved_view',np.array(cv2_img[...,::-1]))
         cv2.waitKey(1)
         
@@ -38,11 +42,11 @@ class CameraSub:
     def camera_subscribe(self):
         
         #frame_rgb= self.sub('/camera/images', Image)  
-        frame_rgb=rospy.Subscriber('/camera/images',Image,self.ros_to_cv2img)
+        #frame_rgb=rospy.Subscriber('/camera/images',Image,self.ros_to_cv2img,queue_size=1)
         
-        #img_msg = rospy.wait_for_message('/camera/images', Image)     
+        self.img_msg = rospy.wait_for_message('/camera/images', Image)     
         #print(img_msg)
-        #frame_rgb=self.ros_to_cv2img(img_msg)
+        frame_rgb=self.ros_to_cv2img(self.img_msg)
         
         return frame_rgb
 
